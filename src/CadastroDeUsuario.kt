a9f6e5ad2dc4f05679479c54343b7b224150a59b
Lucas <lucasgenielsilvagomes@outlook.com>
2022-02-05 21:52:41 -0300

    Exemplo interação com usuário

---
+++ src/CadastroDeUsuario.kt
@@ -0,0 +1,13 @@
+fun main() {
+    println("BEM VINDO ")
+    print("DIGITE SEU NOME; ")
+    var nome = readLine()
+    println("BEM VINDO $nome")
+    print("DIGITE SEU NUMERO;")
+    var numero = readLine()
+    println("Confirma que seu numero de telefone é ; $numero?")
+    var email = readLine()
+    println("eviamos um codigo de confirmação para; $email")
+    var code = readLine()
+    println("digite o $code")
+}
