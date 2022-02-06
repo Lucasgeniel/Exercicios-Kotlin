4bb2af7509f5bdb7ca0cb405659fc394b02f00e7
Lucas <lucasgenielsilvagomes@outlook.com>
2022-02-06 15:17:34 -0300

    Exemplo interação com usuário

--- src/CadastroDeUsuario.kt
+++ src/CadastroDeUsuario.kt
@@ -1,13 +1,21 @@
-fun main() {
-    println("BEM VINDO ")
-    print("DIGITE SEU NOME; ")
-    var nome = readLine()
-    println("BEM VINDO $nome")
-    print("DIGITE SEU NUMERO;")
-    var numero = readLine()
-    println("Confirma que seu numero de telefone é ; $numero?")
-    var email = readLine()
-    println("eviamos um codigo de confirmação para; $email")
-    var code = readLine()
-    println("digite o $code")
+//fun main() {
+//    println("BEM VINDO ")
+ //   print("DIGITE SEU NOME; ")
+//    var nome = readLine()
+ //   println("BEM VINDO $nome")
+//    print("DIGITE SEU NUMERO;")
+ //   var numero = readLine()
+ //   println("Confirma que seu numero de telefone é ; $numero?")
+  //  var email = readLine()
+  //  println("eviamos um codigo de confirmação para; $email")
+ //   var code = readLine()
+  //  println("digite o $code")
+//}
+
+fun main(){
+    var laranja= 3
+    var maça= 2
+
+    var resultado= laranja+ maça
+    println ("$resultado")
 }
--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -1,3 +1,52 @@
 fun main() {
-    println("Hello, world!")
+ var DiasAno = 365
+ var DiasMes = 30
+
+
+ print("informe os anos;")
+ var anos = readLine()
+ print("informe os meses")
+ var meses = readLine()
+ print("informe os days;")
+ var days = readLine()
+
+var resul1= (anos*DiasAno)
+var resul2=(meses*DiasMes)
+
+ var result=days+ resul1+resul2
+ println("sua idade em dias é,$result")
+
 }
+
+private operator fun String?.times(diasAno: Int): Int {
+
+ return diasAno
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
